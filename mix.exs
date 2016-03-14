a47ba6dfd2ebb1b606620425126c517dfc1f6e8a
Asher Van Brunt <asher@okbreathe.com>
2016-03-13 19:08:30 -0700

    Use beta version of ecto

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule Arc.Ecto.Mixfile do
   defp deps do
     [
       {:arc, "~> 0.2"},
-      {:ecto, ">= 0.10.0"},
+      {:ecto, git: "https://github.com/elixir-lang/ecto.git", tag: "v2.0.0-beta.1"},
       {:mock, "~> 0.1.1", only: :test}
     ]
   end
